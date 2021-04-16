5f8bd715f60721c4d486b65736041e81363de11f
Ruts-al <82662641+Ruts-al@users.noreply.github.com>
2021-04-16 17:37:46 +0300

    change js

--- magic.js
+++ magic.js
@@ -1,34 +1,35 @@
-const phrase = ['Да', 'Нет', 'Ты ебанулась такие вопросы задавать?', 'Неодобрительное бурчание', 
-'У тебя все получится!', 'Вернись к этому вопросу позже',
-'Довольно скоро', 'Хорошие перспективы', 'Останешься с голой жопой', 
-'Ты действительно думаешь, что я дам ответ на этот вопрос?', 'Оставь уже этот шар и займись делами', 
-'Будь су4кой', 'Большой брат следит за тобой'
+const phrase = ['Да', 'Нет', 'Ты ебанулась такие вопросы задавать?', 'Неодобрительное бурчание',
+   'У тебя все получится!', 'Вернись к этому вопросу позже',
+   'Довольно скоро', 'Хорошие перспективы', 'Останешься с голой жопой',
+   'Ты действительно думаешь, что я дам ответ на этот вопрос?', 'Оставь уже этот шар и займись делами',
+   'Будь су4кой', 'Большой брат следит за тобой'
 ];
 
- function shake(){
+let isShaking = false;
+function shake() {
+   if (isShaking) return;
+   let ball = document.getElementById("ball")
+   let messageText = document.querySelector(".message")
 
-    let ball= document.getElementById("ball")
-    let messageText = document.querySelector(".message")
- 
-    if(messageText != null){
-     messageText.parentNode.removeChild(messageText);
-    }
-    
-    ball.classList.add("shake");
- 
-    setTimeout(function(){ ball.classList.remove("shake"); }, 1500);
-    
-    setTimeout(function(){ getFortune(); }, 1500);
- }
- 
+   if (messageText != null) {
+      messageText.parentNode.removeChild(messageText);
+   }
 
- function getFortune(){
+   ball.classList.add("shake");
+   isShaking = true;
+   setTimeout(function () { ball.classList.remove("shake"); }, 1500);
 
-     let fortune = phrase[Math.floor(Math.random()*phrase.length)];
- 
-     let parent = document.querySelector(".fortune");
-     let newMessage = document.createElement("div");
-     newMessage.classList.add("message")
-     newMessage.innerHTML = fortune;
-     parent.appendChild(newMessage);
-    } 
+   setTimeout(function () { getFortune(); }, 1500);
+}
+
+
+function getFortune() {
+   isShaking=false;
+   let fortune = phrase[Math.floor(Math.random() * phrase.length)];
+
+   let parent = document.querySelector(".fortune");
+   let newMessage = document.createElement("div");
+   newMessage.classList.add("message")
+   newMessage.innerHTML = fortune;
+   parent.appendChild(newMessage);
+}
